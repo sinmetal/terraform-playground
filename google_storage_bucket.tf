5dbee573f442fdcfe190b893d450ee52b335efcf
sinmetal <metal.tie@gmail.com>
2018-03-28 00:09:13 +0900

    add storage bucket

--- google_storage_bucket.tf
+++ google_storage_bucket.tf
@@ -11,3 +11,10 @@ resource "google_storage_bucket" "sinmetal-terraform-20180327b" {
   location      = "us-central1"
   storage_class = "STANDARD"
 }
+
+resource "google_storage_bucket" "sinmetal-terraform-20180327c" {
+  project       = "sinmetal-terraform"
+  name          = "sinmetal-terraform-20180327c"
+  location      = "us-central1"
+  storage_class = "STANDARD"
+}
