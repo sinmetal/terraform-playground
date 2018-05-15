27cf780054ffd6de332eb285237ae6e5594b09e2
sinmetal <metal.tie@gmail.com>
2018-05-15 12:40:22 +0900

    projectをfolderに入れるように設定した

--- google_project.tf
+++ google_project.tf
@@ -1,5 +1,6 @@
 resource "google_project" "sinmetal-firestore-2" {
-  name = "sinmetal-firestore-2"
+  name       = "sinmetal-firestore-2"
   project_id = "sinmetal-firestore-2"
   org_id     = "69165754818"
+  folder_id  = "${google_folder.playground-firestore.name}"
 }
