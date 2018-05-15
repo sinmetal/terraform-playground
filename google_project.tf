976c9e44bc35da1230c9ea306f663eaa6d69f9ee
sinmetal <metal.tie@gmail.com>
2018-05-15 13:01:44 +0900

    org_idを外した
    'org_id' and 'folder_id' cannot be both set. らしい。

--- google_project.tf
+++ google_project.tf
@@ -1,6 +1,5 @@
 resource "google_project" "sinmetal-firestore-2" {
   name       = "sinmetal-firestore-2"
   project_id = "sinmetal-firestore-2"
-  org_id     = "69165754818"
   folder_id  = "${google_folder.playground-firestore.name}"
 }
