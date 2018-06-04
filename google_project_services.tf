5645b720c2ea3c564713eefff9509fed7e7d1ef1
sinmetal <metal.tie@gmail.com>
2018-06-04 19:40:17 +0900

    実行したらContainer BuilderのAPIがDisableにされて辛かったので、いくつかデフォルトの存在を追加 refs #2

--- google_project_services.tf
+++ google_project_services.tf
@@ -1,4 +1,14 @@
 resource "google_project_services" "project" {
-  project  = "sinmetal-terraform"
-  services = ["iam.googleapis.com", "cloudresourcemanager.googleapis.com", "pubsub.googleapis.com"]
+  project = "sinmetal-terraform"
+
+  services = ["cloudapis.googleapis.com",
+    "iam.googleapis.com",
+    "serviceusage.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "bigquery.googleapis.com",
+    "bigquery-json.googleapis.com",
+    "storage.googleapis.com",
+    "pubsub.googleapis.com",
+  ]
 }
