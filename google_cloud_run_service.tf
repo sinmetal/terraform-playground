94cf2c2654ddabf28a6c345acddc4ef0b445374a
sinmetal <sinmetal@sinmetalcraft.jp>
2021-09-08 20:10:33 +0900

    実はこうか？

--- google_cloud_run_service.tf
+++ google_cloud_run_service.tf
@@ -4,17 +4,16 @@ resource "google_cloud_run_service" "hello" {
   location = "asia-northeast1"
 
   template {
-    metadata {
-      annotations = {
-        "run.googleapis.com/ingress" : "internal-and-cloud-load-balancing"
-      }
-      labels = {
-      }
-    }
     spec {
       containers {
         image = "gcr.io/cloudrun/hello"
       }
+    }
+  }
+
+  metadata {
+    annotations = {
+      "run.googleapis.com/ingress" : "internal-and-cloud-load-balancing"
     }
   }
 }
